8f7386245cbc1239b8b170fbd10f1def08933bee
Juha Järvinen <juha.3.jarvinen@tuni.fi>
2022-04-25 21:34:07 +0300

    Modify navigation bar

--- client/src/components/navigation/MainNavigation.js
+++ client/src/components/navigation/MainNavigation.js
@@ -28,20 +28,20 @@ const MainNavigation = (props) => {
       </SideDrawer>
 
       <MainHeader>
-        <button
-          className="main-navigation__menu-btn"
-          onClick={openDrawerHandler}
-        >
-          <span />
-          <span />
-          <span />
-        </button>
         <h1 className="main-navigation__title">
           <Link to="/">Task Manager</Link>
         </h1>
         <nav className="main-navigation__header-nav">
           <NavLinks />
         </nav>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >☰
+          <span />
+          <span />
+          <span />
+        </button>
       </MainHeader>
     </React.Fragment>
   )
